0abf79838c145a0044e2a3dbe8331f1dae4ef9ab
Jaroslav Kysela <perex@perex.cz>
1998-08-24 16:24:40 +0000

    Removed include/config.h.

--- include/config.h
+++
@@ -1,7 +0,0 @@
-/* include/config.h.  Generated automatically by configure.  */
-/*
- *  Configuration header file for compilation of the ALSA driver
- */
-
-#define SND_LIB_VERSION "0.0.10"
-/* #undef WORDS_BIGENDIAN */
