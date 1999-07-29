57238d2b862a3479273a276fd0e88930a7fafe7c
Jaroslav Kysela <perex@perex.cz>
1999-07-29 13:10:52 +0000

    susumu-o@debian.or.jp - various fixes...

--- include/conv.h
+++ include/conv.h
@@ -14,7 +14,7 @@
 #define snd_host_to_BE_16(val)	((((val)>>8)&0xff)|(((val)<<8)&0xff00))
 #define snd_BE_to_host_16(val)	snd_host_to_BE_16(val)
 #define snd_host_to_BE_32(val)	((((val)>>24)&0xff)|(((val)>>16)&0xff00)|\
-                                 (((val)<<16)&0xff0000)|(((val)<<24)&0xff000000)
+                                 (((val)<<16)&0xff0000)|(((val)<<24)&0xff000000))
 #define snd_BE_to_host_32(val)	snd_host_to_BE_32(val)
 
 #else                                      
@@ -24,10 +24,10 @@
 #define snd_host_to_BE_16(val)	(val)
 #define snd_BE_to_host_16(val)	(val)
 #define snd_host_to_LE_16(val)	((((val)>>8)&0xff)|(((val)<<8)&0xff00))
-#define snd_LE_to_host_16(val)	snd_host_to_BE_16(val)
+#define snd_LE_to_host_16(val)	snd_host_to_LE_16(val)
 #define snd_host_to_LE_32(val)	((((val)>>24)&0xff)|(((val)>>16)&0xff00)|\
-                                 (((val)<<16)&0xff0000)|(((val)<<24)&0xff000000)
-#define snd_LE_to_host_32(val)	snd_host_to_BE_32(val)
+                                 (((val)<<16)&0xff0000)|(((val)<<24)&0xff000000))
+#define snd_LE_to_host_32(val)	snd_host_to_LE_32(val)
 
 #endif
 
