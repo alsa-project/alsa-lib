a9e578d63c9105082ee9eed8bafdb711158e4b94
Takashi Iwai <tiwai@suse.de>
2002-10-07 09:57:31 +0000

    get back for the old autoheader.

---
+++ acconfig.h
@@ -0,0 +1,20 @@
+/* Package name */
+#undef PACKAGE
+
+/* Package version */
+#undef VERSION
+
+/* Sound library version string */
+#undef SND_LIB_VERSION
+
+/* DATA directory */
+#undef DATADIR
+
+/* NDEBUG */
+#undef NDEBUG
+
+/* Do we have FPU on this machine? */
+#undef HAVE_SOFT_FLOAT
+
+/* Use versioned symbols for shared library? */
+#undef VERSIONED_SYMBOLS
