9db1403b3addd31d5b733a2ee83a81bb57acbeb4
Takashi Iwai <tiwai@suse.de>
2004-09-21 10:37:17 +0000

    [hdsp] Fix for 64bit architectures
    Fixed the loading of firmware data and the handling of meter mmap
    on 64bit architectures.

--- include/sound/hdsp.h
+++ include/sound/hdsp.h
@@ -76,7 +76,7 @@ struct _snd_hdsp_config_info {
 typedef struct _snd_hdsp_firmware hdsp_firmware_t;
 
 struct _snd_hdsp_firmware {
-	unsigned long *firmware_data;	/* 24413 long words */
+	void *firmware_data;	/* 24413 x 4 bytes */
 };
 
 #define SNDRV_HDSP_IOCTL_UPLOAD_FIRMWARE _IOW('H', 0x42, hdsp_firmware_t)
