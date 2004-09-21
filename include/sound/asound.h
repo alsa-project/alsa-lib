6ed5c58dceee7cb4fa7f0e75d20bc7c94174de7f
Takashi Iwai <tiwai@suse.de>
2004-09-21 10:36:54 +0000

    Remove the obsolete NONATOMIC ops bit.
    SNDRV_PCM_INFO_NONATOMIC_OPS bit is removed.  It's obsolete now.

--- include/sound/asound.h
+++ include/sound/asound.h
@@ -273,7 +273,6 @@ enum sndrv_pcm_subformat {
 #define SNDRV_PCM_INFO_HALF_DUPLEX	0x00100000	/* only half duplex */
 #define SNDRV_PCM_INFO_JOINT_DUPLEX	0x00200000	/* playback and capture stream are somewhat correlated */
 #define SNDRV_PCM_INFO_SYNC_START	0x00400000	/* pcm support some kind of sync go */
-#define SNDRV_PCM_INFO_NONATOMIC_OPS	0x00800000	/* non-atomic prepare callback */
 
 enum sndrv_pcm_state {
 	SNDRV_PCM_STATE_OPEN = 0,	/* stream is open */
