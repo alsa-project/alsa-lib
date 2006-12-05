6d90397addbbc418e5c6745c56054ed4f89e31bb
Jaroslav Kysela <perex@perex.cz>
2006-12-05 11:52:37 +0100

    add snd_device_name_get_hint() prototype to include/control.h

--- include/control.h
+++ include/control.h
@@ -216,6 +216,7 @@ int snd_card_get_longname(int card, char **name);
 
 int snd_device_name_hint(int card, const char *iface, void ***hints);
 int snd_device_name_free_hint(void **hints);
+char *snd_device_name_get_hint(const void *hint, const char *id);
 
 int snd_ctl_open(snd_ctl_t **ctl, const char *name, int mode);
 int snd_ctl_open_lconf(snd_ctl_t **ctl, const char *name, int mode, snd_config_t *lconf);
