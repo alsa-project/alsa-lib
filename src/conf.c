e2317969e140b2c956db38583dcc0d7f3747670f
Clemens Ladisch <clemens@ladisch.de>
2009-07-10 07:25:50 +0200

    conf.c: snd_config_set_id: prevent duplicate ids
    snd_config_add() checks for duplicate ids, but it was possible to create
    duplicates by adding a note and changing the id afterwards with
    snd_config_set_id(); so we have to add a check there, too.
    
    Signed-off-by: Clemens Ladisch <clemens@ladisch.de>

--- src/conf.c
+++ src/conf.c
@@ -1521,9 +1521,17 @@ int snd_config_get_id(const snd_config_t *config, const char **id)
  */
 int snd_config_set_id(snd_config_t *config, const char *id)
 {
+	snd_config_iterator_t i, next;
 	char *new_id;
 	assert(config);
 	if (id) {
+		if (config->father) {
+			snd_config_for_each(i, next, config->father) {
+				snd_config_t *n = snd_config_iterator_entry(i);
+				if (n != config && strcmp(id, n->id) == 0)
+					return -EEXIST;
+			}
+		}
 		new_id = strdup(id);
 		if (!new_id)
 			return -ENOMEM;
