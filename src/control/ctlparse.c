1720d30ad82ebb8b5f52fc08c6c610e51203fa89
Alexander Stein <a.stein@systec-electronic.com>
2011-05-19 15:16:36 +0200

    ctlparse: Respect softfloat configure option
    If we want softlfoat we can't use ceil which uses libm.
    
    Signed-off-by: Alexander Stein <a.stein@systec-electronic.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- src/control/ctlparse.c
+++ src/control/ctlparse.c
@@ -33,8 +33,17 @@
 
 /* Function to convert from percentage to volume. val = percentage */
 
+#ifdef HAVE_SOFT_FLOAT
+static inline long int convert_prange1(long val, long min, long max)
+{
+	long temp = val * (max - min);
+	return temp / 100 + min + ((temp % 100) == 0 ? 0 : 1);
+}
+#else
+
 #define convert_prange1(val, min, max) \
 	ceil((val) * ((max) - (min)) * 0.01 + (min))
+#endif
 
 #define check_range(val, min, max) \
 	((val < min) ? (min) : ((val > max) ? (max) : (val)))
