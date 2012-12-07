1b428e7719fdec1b5e3206b8e55a86c4891d1849
Takashi Iwai <tiwai@suse.de>
2012-12-07 09:28:06 +0100

    mixer: Don't build simple_abst when no libdl is available
    Check BUILD_MODULES conditional not to compile simple_abst.c.
    Also return -ENXIO to caller statically in that case.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- src/mixer/mixer_simple.h
+++ src/mixer/mixer_simple.h
@@ -28,4 +28,14 @@
 	snd1_mixer_simple_basic_register
 
 int snd_mixer_simple_none_register(snd_mixer_t *mixer, struct snd_mixer_selem_regopt *options, snd_mixer_class_t **classp);
+
+#ifdef HAVE_LIBDL
 int snd_mixer_simple_basic_register(snd_mixer_t *mixer, struct snd_mixer_selem_regopt *options, snd_mixer_class_t **classp);
+#else
+static inline int snd_mixer_simple_basic_register(snd_mixer_t *mixer,
+						  struct snd_mixer_selem_regopt *options,
+						  snd_mixer_class_t **classp)
+{
+	return -ENXIO;
+}
+#endif
