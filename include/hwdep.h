dfc3bf97bf45bd78d498d20fcf930541350f836d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2014-08-18 18:45:17 +0900

    Sync enum snd_hwdep_iface_t with include/asound/asound.h
    Some members in this enumerated type has not updated for 9 years, although
    kernel-drivers added them during this period. This commit adds them following
    to a commit 87df9f3 'sync include/asound/asound.h with 3.17-rc1 kernel'.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/hwdep.h
+++ include/hwdep.h
@@ -68,8 +68,13 @@ typedef enum _snd_hwdep_iface {
 	SND_HWDEP_IFACE_USX2Y_PCM,	/**< Tascam US122, US224 & US428 raw USB PCM */
 	SND_HWDEP_IFACE_PCXHR,		/**< Digigram PCXHR */
 	SND_HWDEP_IFACE_SB_RC,		/**< SB Extigy/Audigy2NX remote control */
-                
-	SND_HWDEP_IFACE_LAST = SND_HWDEP_IFACE_SB_RC  /**< last known hwdep interface */
+	SND_HWDEP_IFACE_HDA,		/**< HD-audio */
+	SND_HWDEP_IFACE_USB_STREAM,	/**< direct access to usb stream */
+	SND_HWDEP_IFACE_FW_DICE,	/**< TC DICE FireWire device */
+	SND_HWDEP_IFACE_FW_FIREWORKS,	/**< Echo Audio Fireworks based device */
+	SND_HWDEP_IFACE_FW_BEBOB,	/**< BridgeCo BeBoB based device */
+
+	SND_HWDEP_IFACE_LAST = SND_HWDEP_IFACE_FW_BEBOB  /**< last known hwdep interface */
 } snd_hwdep_iface_t;
 
 /** open for reading */
