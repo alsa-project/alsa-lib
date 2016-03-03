0e3e574012b042145b97a68f026f005248641fbb
Thomas Klausner <wiz@NetBSD.org>
2016-03-03 12:57:34 +0100

    Only use F_SETSIG fcntl where it exists.
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- src/timer/timer_hw.c
+++ src/timer/timer_hw.c
@@ -85,10 +85,12 @@ static int snd_timer_hw_async(snd_timer_t *timer, int sig, pid_t pid)
 	}
 	if (sig < 0)
 		return 0;
+#ifdef F_SETSIG
 	if (fcntl(fd, F_SETSIG, (long)sig) < 0) {
 		SYSERR("F_SETSIG failed");
 		return -errno;
 	}
+#endif
 	if (fcntl(fd, F_SETOWN, (long)pid) < 0) {
 		SYSERR("F_SETOWN failed");
 		return -errno;
