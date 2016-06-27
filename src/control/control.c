7bc886584f30e94c817cdfa0857168e50b189f45
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2016-06-27 22:02:57 +0900

    ctl: fix returning zero for dimension level 4
    In ALSA kernel/userspace interfaces, 'struct snd_ctl_elem_info' has a
    'dimension' parameter. This parameter consists of an array with four
    elements. Each element represents the number of members in corresponding
    dimension level to construct matrix.
    
    In current implementation, a get function, 'snd_ctl_elem_info_get_dimension()'
    is hardcoded to return zero to level 4, against actual value. This commit fixes
    the bug.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- src/control/control.c
+++ src/control/control.c
@@ -2483,7 +2483,7 @@ int snd_ctl_elem_info_get_dimension(const snd_ctl_elem_info_t *obj, unsigned int
 #endif
 {
 	assert(obj);
-	if (idx >= 3)
+	if (idx > 3)
 		return 0;
 	return obj->dimen.d[idx];
 }
