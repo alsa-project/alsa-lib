60875a1ca6a2b4eafda4a82afcdd724953545d92
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2016-07-15 09:23:18 +0900

    mixer: remove alloca() from elem_write_switch_constant()
    Both of alloca() and automatic variables keep storages on stack, while
    the former generates more instructions than the latter. It's better to use
    the latter if the size of storage is computable at pre-compile or compile
    time; i.e. just for structures.
    
    This commit obsolete usages of alloca() with automatic variables.
    
    Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- src/mixer/simple_none.c
+++ src/mixer/simple_none.c
@@ -499,16 +499,15 @@ static int elem_write_switch(selem_none_t *s, int dir, selem_ctl_type_t type)
 
 static int elem_write_switch_constant(selem_none_t *s, selem_ctl_type_t type, int val)
 {
-	snd_ctl_elem_value_t *ctl;
+	snd_ctl_elem_value_t ctl = {0};
 	unsigned int idx;
 	int err;
 	selem_ctl_t *c = &s->ctls[type];
-	snd_ctl_elem_value_alloca(&ctl);
-	if ((err = snd_hctl_elem_read(c->elem, ctl)) < 0)
+	if ((err = snd_hctl_elem_read(c->elem, &ctl)) < 0)
 		return err;
 	for (idx = 0; idx < c->values; idx++)
-		snd_ctl_elem_value_set_integer(ctl, idx, !!val);
-	if ((err = snd_hctl_elem_write(c->elem, ctl)) < 0)
+		snd_ctl_elem_value_set_integer(&ctl, idx, !!val);
+	if ((err = snd_hctl_elem_write(c->elem, &ctl)) < 0)
 		return err;
 	return 0;
 }
