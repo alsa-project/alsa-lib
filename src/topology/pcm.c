alsa-project/alsa-lib

commit 53f7711769d446612db40bffdff5bc8d2fbf7db7

topology: Define a function to build a single physical DAI link
Code refactoring. Rename the function to build all physical links from
tplg_build_link_config() to tplg_build_links(). And define a new function
build_link() to build a single physical DAI link element.

The function build_link() will be extended to handle more properties
of a physical DAI link (e.g. backend or codec-codec link).

Signed-off-by: Mengdong Lin <mengdong.lin@linux.intel.com>
Signed-off-by: Takashi Iwai <tiwai@suse.de>

src/topology/pcm.c
@@ -148,12 +148,31 @@ static int tplg_build_stream_cfg(snd_tplg_t *tplg,
 	return 0;
 }
 
+static int build_link(snd_tplg_t *tplg, struct tplg_elem *elem)
+{
+	struct snd_soc_tplg_link_config *link = elem->link;
+	struct tplg_elem *ref_elem = NULL;
+	struct snd_soc_tplg_link_cmpnt  *codec, *cmpnt;
+	struct tplg_ref *ref;
+	struct list_head *base, *pos;
+	int i, num_hw_configs = 0, err = 0;
+
+	err = tplg_build_stream_cfg(tplg, link->stream,
+				    link->num_streams);
+	if (err < 0)
+		return err;
+
+	/* add link to manifest */
+	tplg->manifest.dai_link_elems++;
+
+	return 0;
+}
+
 /* build BE/CC DAI link configurations */
-int tplg_build_link_cfg(snd_tplg_t *tplg, unsigned int type)
+int tplg_build_links(snd_tplg_t *tplg, unsigned int type)
 {
 	struct list_head *base, *pos;
 	struct tplg_elem *elem;
-	struct snd_soc_tplg_link_config *link;
 	int err = 0;
 
 	switch (type) {
@@ -175,9 +194,7 @@ int tplg_build_link_cfg(snd_tplg_t *tplg, unsigned int type)
 			return -EINVAL;
 		}
 
-		link = elem->link;
-		err = tplg_build_stream_cfg(tplg, link->stream,
-			link->num_streams);
+		err =  build_link(tplg, elem);
 		if (err < 0)
 			return err;
 	}

src/topology/parser.c
@@ -271,11 +271,11 @@ static int tplg_build_integ(snd_tplg_t *tplg)
 	if (err <  0)
 		return err;
 
-	err = tplg_build_link_cfg(tplg, SND_TPLG_TYPE_BE);
+	err = tplg_build_links(tplg, SND_TPLG_TYPE_BE);
 	if (err <  0)
 		return err;
 
-	err = tplg_build_link_cfg(tplg, SND_TPLG_TYPE_CC);
+	err = tplg_build_links(tplg, SND_TPLG_TYPE_CC);
 	if (err <  0)
 		return err;
 

src/topology/tplg_local.h
@@ -284,6 +284,6 @@ int tplg_add_bytes(snd_tplg_t *tplg, struct snd_tplg_bytes_template *bytes_ctl,
 		   struct tplg_elem **e);
 
 int tplg_build_pcms(snd_tplg_t *tplg, unsigned int type);
-int tplg_build_link_cfg(snd_tplg_t *tplg, unsigned int type);
+int tplg_build_links(snd_tplg_t *tplg, unsigned int type);
 int tplg_add_link_object(snd_tplg_t *tplg, snd_tplg_obj_template_t *t);
 int tplg_add_pcm_object(snd_tplg_t *tplg, snd_tplg_obj_template_t *t);

commit 5a2bddfc79912d7537a3a43e28481bfefa9db88e

topology: fix unused-variable warnings introduced to build_link()
A function with unused automatic variables was recently introduced to this
library, then compiler generates below warnings.

pcm.c: In function ‘build_link’:
pcm.c:213:6: warning: unused variable ‘i’ [-Wunused-variable]
  int i, num_hw_configs = 0, err = 0;
      ^
pcm.c:210:43: warning: unused variable ‘cmpnt’ [-Wunused-variable]
  struct snd_soc_tplg_link_cmpnt  *codec, *cmpnt;
                                           ^~~~~
pcm.c:210:35: warning: unused variable ‘codec’ [-Wunused-variable]
  struct snd_soc_tplg_link_cmpnt  *codec, *cmpnt;
                                   ^~~~~
pcm.c:209:20: warning: unused variable ‘ref_elem’ [-Wunused-variable]
  struct tplg_elem *ref_elem = NULL;

This commit removes them.

Fixes: 53f7711769d4("topology: Define a function to build a single physical DAI link")
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
Signed-off-by: Takashi Iwai <tiwai@suse.de>

src/topology/pcm.c
@@ -206,11 +206,9 @@ static int tplg_build_stream_cfg(snd_tplg_t *tplg,
 static int build_link(snd_tplg_t *tplg, struct tplg_elem *elem)
 {
 	struct snd_soc_tplg_link_config *link = elem->link;
-	struct tplg_elem *ref_elem = NULL;
-	struct snd_soc_tplg_link_cmpnt  *codec, *cmpnt;
 	struct tplg_ref *ref;
 	struct list_head *base, *pos;
-	int i, num_hw_configs = 0, err = 0;
+	int num_hw_configs = 0, err = 0;
 
 	err = tplg_build_stream_cfg(tplg, link->stream,
 				    link->num_streams);