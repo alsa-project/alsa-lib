3f0dc404f16af58d20b4489b0daafcf87555dfb7
Mounesh Sutar <sutar.mounesh@gmail.com>
2017-01-10 12:03:17 +0530

    pcm: direct: returning semop error code for semaphore up/down failures
    Signed-off-by: Mounesh Sutar <sutar.mounesh@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- src/pcm/pcm_direct.h
+++ src/pcm/pcm_direct.h
@@ -263,7 +263,10 @@ static inline int snd_pcm_direct_semaphore_down(snd_pcm_direct_t *dmix, int sem_
 {
 	struct sembuf op[2] = { { sem_num, 0, 0 }, { sem_num, 1, SEM_UNDO } };
 	int err = semop(dmix->semid, op, 2);
-	if (err == 0) dmix->locked[sem_num]++;
+	if (err == 0)
+		dmix->locked[sem_num]++;
+	else if (err == -1)
+		err = -errno;
 	return err;
 }
 
@@ -271,7 +274,10 @@ static inline int snd_pcm_direct_semaphore_up(snd_pcm_direct_t *dmix, int sem_nu
 {
 	struct sembuf op = { sem_num, -1, SEM_UNDO | IPC_NOWAIT };
 	int err = semop(dmix->semid, &op, 1);
-	if (err == 0) dmix->locked[sem_num]--;
+	if (err == 0)
+		dmix->locked[sem_num]--;
+	else if (err == -1)
+		err = -errno;
 	return err;
 }
 
