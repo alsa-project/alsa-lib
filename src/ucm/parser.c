alsa-project/alsa-lib

commit 77119d83a1f42b648671cf856e699500d89dc651

ucm: Fix fallback to card_name (shortname) config for ucm1 profiles
uc_mgr_import_master_config() first calls load_master_config()
with the card's longname and if that fails then calls it again with the
card_name.

Before this commit configuration_filename() would force conf_format to 2
when it the access(fn, R_OK) test failed for the ucm1 longname
master-config filename.

This would cause configuration_filename() to blindly return a filename
under <prefix>/ucm2 without seeing if that is actually there and without
trying to fallback to the old profiles under <prefix>/ucm, breaking the
loading of UCM1 profiles by card_name.

This commit fixes this by modifying configuration_filename() to not set
conf_format when checking for the UCM1 config filename fails.
Instead, to make sure that any errors about opening the file still report
the new path, configuration_filename() now resets the filename to the UCM2
path if the access() check has failed for both the UCM2 and UCM1 paths,
without touching conf_format.

Fixes: aba2260ae7b5 ("ucm: switch to ucm2 directory and v2 format, keep backward compatibility")
Signed-off-by: Hans de Goede <hdegoede@redhat.com>
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/ucm/parser.c
@@ -101,7 +101,6 @@ static void configuration_filename(snd_use_case_mgr_t *uc_mgr,
 	}
 
 	if (uc_mgr->conf_format > 0) {
-__format:
 		configuration_filename2(fn, fn_len, uc_mgr->conf_format,
 					dir, file, suffix);
 		return;
@@ -112,11 +111,11 @@ __format:
 		return;
 
 	configuration_filename2(fn, fn_len, 0, dir, file, suffix);
-	if (access(fn, R_OK)) {
-		/* make sure that the error message refers to the new path */
-		uc_mgr->conf_format = 2;
-		goto __format;
-	}
+	if (access(fn, R_OK) == 0)
+		return;
+
+	/* make sure that the error message refers to the new path */
+	configuration_filename2(fn, fn_len, 2, dir, file, suffix);
 }
 
 /*

commit 10a63e093c4a98acfa9bcdfdd06938bcd874b008

ucm: fix again the long name configuration filename lookup
The fix in commit 77119d83a1f4 assumes that both ucm1 / ucm2 directories
have the configurations for the long name in the same directory. For v2
we changed the location to card_name/card_long_name aka
driver_name/driver_long_name to make the directory layout more structured.

Fixes: 77119d83a1f4 ("ucm: Fix opening of master-configs by the card's longname")

Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/ucm/parser.c
@@ -1567,7 +1567,7 @@ static int get_by_card(snd_use_case_mgr_t *mgr, const char *ctl_name, char *long
 }
 
 static int load_master_config(snd_use_case_mgr_t *uc_mgr,
-			      const char *card_name, snd_config_t **cfg, int fcheck)
+			      const char *card_name, snd_config_t **cfg, int longname)
 {
 	char filename[MAX_FILE];
 	int err;
@@ -1578,12 +1578,25 @@ static int load_master_config(snd_use_case_mgr_t *uc_mgr,
 		return -EINVAL;
 	}
 
-	configuration_filename(uc_mgr, filename, sizeof(filename),
-			       card_name, card_name, ".conf");
-
-	/* if the configuration file does not exist, silently return */
-	if (fcheck && access(filename, R_OK) != 0)
-		return -ENOENT;
+	uc_mgr->conf_format = 0;
+	if (longname) {
+		if (getenv(ALSA_CONFIG_UCM2_VAR) || !getenv(ALSA_CONFIG_UCM_VAR)) {
+			uc_mgr->conf_format = 2;
+			configuration_filename(uc_mgr, filename, sizeof(filename),
+					       uc_mgr->conf_file_name, card_name, ".conf");
+		}
+		if (uc_mgr->conf_format >= 2 && access(filename, R_OK) != 0) {
+			/* try the old ucm directory */
+			uc_mgr->conf_format = 1;
+			configuration_filename(uc_mgr, filename, sizeof(filename),
+					       card_name, card_name, ".conf");
+			if (access(filename, R_OK) != 0)
+				return -ENOENT;
+		}
+	} else {
+		configuration_filename(uc_mgr, filename, sizeof(filename),
+				       card_name, card_name, ".conf");
+	}
 
 	err = uc_mgr_config_load(uc_mgr->conf_format, filename, cfg);
 	if (err < 0) {