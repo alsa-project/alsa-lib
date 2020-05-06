bb43546c7a3d535576b49f0a729761ba6a348892
Takashi Iwai <tiwai@suse.de>
2020-05-06 22:40:16 +0200

    header: Fix build error with old kernel headers
    Old development environment doesn't necessarily contain the headers
    defining __kernel_long_t that is recently used for y2038 timespec
    conditional.  Define it explicitly in such a case.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/sound/asound.h
+++ include/sound/asound.h
@@ -1,1 +1,11 @@
+/* workaround for building with old glibc / kernel headers */
+#ifdef __linux__
+#include <linux/types.h>
+#else
+#include <sys/types.h>
+#endif
+#ifndef __kernel_long_t
+#define __kernel_long_t long
+#endif
+
 #include <alsa/sound/uapi/asound.h>
