alsa-project/alsa-lib

commit 51e1d486ce4535923692bd1d7e59d5aa3a29cd50

topology: parse_tuple_set() - remove dead condition code
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/topology/data.c
@@ -860,7 +860,7 @@ static int parse_tuple_set(snd_config_t *cfg,
 			}
 
 			if ((type == SND_SOC_TPLG_TUPLE_TYPE_WORD
-					&& tuple_val > UINT_MAX)
+					/* && tuple_val > UINT_MAX */)
 				|| (type == SND_SOC_TPLG_TUPLE_TYPE_SHORT
 					&& tuple_val > USHRT_MAX)
 				|| (type == SND_SOC_TPLG_TUPLE_TYPE_BYTE

commit ae7362c996e0b991c399cc26c6d0b3c8e295e83c

topology: fix 'parse_tuple_set() - remove dead condition code'
The whole SND_SOC_TPLG_TUPLE_TYPE_WORD condition part must be
commented out, otherwise the condition is always true.

Fixes: 51e1d486ce ("topology: parse_tuple_set() - remove dead condition code")
BugLink: https://github.com/alsa-project/alsa-lib/issues/114
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/topology/data.c
@@ -859,11 +859,11 @@ static int parse_tuple_set(snd_config_t *cfg,
 				goto err;
 			}
 
-			if ((type == SND_SOC_TPLG_TUPLE_TYPE_WORD
-					/* && tuple_val > UINT_MAX */)
-				|| (type == SND_SOC_TPLG_TUPLE_TYPE_SHORT
-					&& tuple_val > USHRT_MAX)
-				|| (type == SND_SOC_TPLG_TUPLE_TYPE_BYTE
+			if (/* (type == SND_SOC_TPLG_TUPLE_TYPE_WORD
+					&& tuple_val > UINT_MAX) || */
+				(type == SND_SOC_TPLG_TUPLE_TYPE_SHORT
+					&& tuple_val > USHRT_MAX) ||
+				(type == SND_SOC_TPLG_TUPLE_TYPE_BYTE
 					&& tuple_val > UCHAR_MAX)) {
 				SNDERR("tuple %s: invalid value", id);
 				goto err;