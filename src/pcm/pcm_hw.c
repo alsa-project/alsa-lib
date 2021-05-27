alsa-project/alsa-lib

commit c7a939ee31a8a25253d9620ecea6620d48a028eb

pcm: hw - fix again snd_pcm_hw_state() when the driver was disconnected
Fixes: 2c8e31f5 ("pcm: hw - fix snd_pcm_hw_state()")
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/pcm/pcm_hw.c
@@ -600,9 +600,10 @@ static int snd_pcm_hw_status(snd_pcm_t *pcm, snd_pcm_status_t * status)
 static snd_pcm_state_t snd_pcm_hw_state(snd_pcm_t *pcm)
 {
 	snd_pcm_hw_t *hw = pcm->private_data;
-	/* no error checking, the mmap state should be correct
-	 * after SNDRV_PCM_IOCTL_SYNC_PTR */
-	query_status_data(hw);
+	/* the -ENODEV may come from the snd_disconnect_ioctl() in kernel */
+	/* in this case, the mmaped state is no longer updated */
+	if (query_status_data(hw) == -ENODEV)
+		return SND_PCM_STATE_DISCONNECTED;
 	return (snd_pcm_state_t) hw->mmap_status->state;
 }
 

commit 1e56b1031f59ab5a28cbc648e594d67de0fad90b

pcm: hw - correct the comment in snd_pcm_hw_state()
Fixes: c7a939ee ("pcm: hw - fix again snd_pcm_hw_state() when the driver was disconnected")
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/pcm/pcm_hw.c
@@ -600,8 +600,8 @@ static int snd_pcm_hw_status(snd_pcm_t *pcm, snd_pcm_status_t * status)
 static snd_pcm_state_t snd_pcm_hw_state(snd_pcm_t *pcm)
 {
 	snd_pcm_hw_t *hw = pcm->private_data;
-	/* the -ENODEV may come from the snd_disconnect_ioctl() in kernel */
-	/* in this case, the mmaped state is no longer updated */
+	/* the -ENODEV may come from the snd_disconnect_ioctl() or
+	   snd_power_wait() in kernel */
 	if (query_status_data(hw) == -ENODEV)
 		return SND_PCM_STATE_DISCONNECTED;
 	return (snd_pcm_state_t) hw->mmap_status->state;