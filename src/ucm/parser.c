e2133090603a74c0b08cd45b689063071bc90c81
Jaroslav Kysela <perex@perex.cz>
2021-06-03 07:30:27 +0200

    ucm: fix possible memory leak in parse_verb_file()
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- src/ucm/parser.c
+++ src/ucm/parser.c
@@ -1779,7 +1779,7 @@ static int parse_verb_file(snd_use_case_mgr_t *uc_mgr,
 			err = parse_libconfig(uc_mgr, n);
 			if (err < 0) {
 				uc_error("error: failed to parse LibConfig");
-				return err;
+				goto _err;
 			}
 			continue;
 		}
