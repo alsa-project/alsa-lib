f1fa7ea07768bd5dbe15462ef734b28380a12a43
Jaroslav Kysela <perex@perex.cz>
2022-05-05 14:37:52 +0200

    control: shm - initialize write buffer
      ccontrol_shm.c: In function ‘snd_ctl_shm_action’:
      control_shm.c:54:23: error: invalid initializer
         54 |         char buf[1] = 0;
            |                       ^
    
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- src/control/control_shm.c
+++ src/control/control_shm.c
@@ -51,7 +51,7 @@ static int snd_ctl_shm_action(snd_ctl_t *ctl)
 {
 	snd_ctl_shm_t *shm = ctl->private_data;
 	int err;
-	char buf[1];
+	char buf[1] = {0};
 	volatile snd_ctl_shm_ctrl_t *ctrl = shm->ctrl;
 	err = write(shm->socket, buf, 1);
 	if (err != 1)
