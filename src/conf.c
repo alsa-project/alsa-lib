469851c6bd19b797ca4bf69e711f3503c2b8d157
Maxython <mixython@gmail.com>
2023-02-10 23:00:51 +0300

    fix detection of android devices

--- src/conf.c
+++ src/conf.c
@@ -4116,7 +4116,7 @@ static int config_file_load(snd_config_t *root, const char *fn, int errors)
 	if (!S_ISDIR(st.st_mode))
 		return config_file_open(root, fn);
 #ifndef DOC_HIDDEN
-#if defined(_GNU_SOURCE) && !defined(__NetBSD__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__DragonFly__) && !defined(__sun) && !defined(ANDROID)
+#if defined(_GNU_SOURCE) && !defined(__NetBSD__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__DragonFly__) && !defined(__sun) && !defined(__ANDROID__)
 #define SORTFUNC	versionsort64
 #else
 #define SORTFUNC	alphasort64
--- src/ucm/parser.c
+++ src/ucm/parser.c
@@ -2915,7 +2915,7 @@ int uc_mgr_scan_master_configs(const char **_list[])
 		snprintf(filename, sizeof(filename), "%s/ucm2/conf.virt.d",
 			 snd_config_topdir());
 
-#if defined(_GNU_SOURCE) && !defined(__NetBSD__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__DragonFly__) && !defined(__sun) && !defined(ANDROID)
+#if defined(_GNU_SOURCE) && !defined(__NetBSD__) && !defined(__FreeBSD__) && !defined(__OpenBSD__) && !defined(__DragonFly__) && !defined(__sun) && !defined(__ANDROID__)
 #define SORTFUNC	versionsort64
 #else
 #define SORTFUNC	alphasort64
