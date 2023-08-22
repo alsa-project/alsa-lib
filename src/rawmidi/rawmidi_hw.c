403d687b95d9ac1008ef03ca3e1aa0b63c4aaff6
Takashi Iwai <tiwai@suse.de>
2023-08-22 14:29:03 +0200

    rawmidi: Suppress error messages for non-fatal errors
    Align the behavior of rawmidi errors at open & co with PCM, i.e. use
    SYSMSG() instead of SYSERR() for suppressing the error messages as
    default.
    
    Closes: https://github.com/alsa-project/alsa-lib/issues/344
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- src/rawmidi/rawmidi_hw.c
+++ src/rawmidi/rawmidi_hw.c
@@ -68,7 +68,7 @@ static int snd_rawmidi_hw_close(snd_rawmidi_t *rmidi)
 		return 0;
 	if (close(hw->fd)) {
 		err = -errno;
-		SYSERR("close failed\n");
+		SYSMSG("close failed");
 	}
 	free(hw->buf);
 	free(hw);
@@ -81,7 +81,7 @@ static int snd_rawmidi_hw_nonblock(snd_rawmidi_t *rmidi, int nonblock)
 	long flags;
 
 	if ((flags = fcntl(hw->fd, F_GETFL)) < 0) {
-		SYSERR("F_GETFL failed");
+		SYSMSG("F_GETFL failed");
 		return -errno;
 	}
 	if (nonblock)
@@ -89,7 +89,7 @@ static int snd_rawmidi_hw_nonblock(snd_rawmidi_t *rmidi, int nonblock)
 	else
 		flags &= ~O_NONBLOCK;
 	if (fcntl(hw->fd, F_SETFL, flags) < 0) {
-		SYSERR("F_SETFL for O_NONBLOCK failed");
+		SYSMSG("F_SETFL for O_NONBLOCK failed");
 		return -errno;
 	}
 	return 0;
@@ -100,7 +100,7 @@ static int snd_rawmidi_hw_info(snd_rawmidi_t *rmidi, snd_rawmidi_info_t * info)
 	snd_rawmidi_hw_t *hw = rmidi->private_data;
 	info->stream = rmidi->stream;
 	if (ioctl(hw->fd, SNDRV_RAWMIDI_IOCTL_INFO, info) < 0) {
-		SYSERR("SNDRV_RAWMIDI_IOCTL_INFO failed");
+		SYSMSG("SNDRV_RAWMIDI_IOCTL_INFO failed");
 		return -errno;
 	}
 	return 0;
@@ -112,7 +112,7 @@ static int snd_rawmidi_hw_params(snd_rawmidi_t *rmidi, snd_rawmidi_params_t * pa
 	int tstamp;
 	params->stream = rmidi->stream;
 	if (ioctl(hw->fd, SNDRV_RAWMIDI_IOCTL_PARAMS, params) < 0) {
-		SYSERR("SNDRV_RAWMIDI_IOCTL_PARAMS failed");
+		SYSMSG("SNDRV_RAWMIDI_IOCTL_PARAMS failed");
 		return -errno;
 	}
 	buf_reset(hw);
@@ -144,7 +144,7 @@ static int snd_rawmidi_hw_status(snd_rawmidi_t *rmidi, snd_rawmidi_status_t * st
 	snd_rawmidi_hw_t *hw = rmidi->private_data;
 	status->stream = rmidi->stream;
 	if (ioctl(hw->fd, SNDRV_RAWMIDI_IOCTL_STATUS, status) < 0) {
-		SYSERR("SNDRV_RAWMIDI_IOCTL_STATUS failed");
+		SYSMSG("SNDRV_RAWMIDI_IOCTL_STATUS failed");
 		return -errno;
 	}
 	return 0;
@@ -155,7 +155,7 @@ static int snd_rawmidi_hw_drop(snd_rawmidi_t *rmidi)
 	snd_rawmidi_hw_t *hw = rmidi->private_data;
 	int str = rmidi->stream;
 	if (ioctl(hw->fd, SNDRV_RAWMIDI_IOCTL_DROP, &str) < 0) {
-		SYSERR("SNDRV_RAWMIDI_IOCTL_DROP failed");
+		SYSMSG("SNDRV_RAWMIDI_IOCTL_DROP failed");
 		return -errno;
 	}
 	buf_reset(hw);
@@ -167,7 +167,7 @@ static int snd_rawmidi_hw_drain(snd_rawmidi_t *rmidi)
 	snd_rawmidi_hw_t *hw = rmidi->private_data;
 	int str = rmidi->stream;
 	if (ioctl(hw->fd, SNDRV_RAWMIDI_IOCTL_DRAIN, &str) < 0) {
-		SYSERR("SNDRV_RAWMIDI_IOCTL_DRAIN failed");
+		SYSMSG("SNDRV_RAWMIDI_IOCTL_DRAIN failed");
 		return -errno;
 	}
 	return 0;
@@ -381,13 +381,13 @@ int snd_rawmidi_hw_open(snd_rawmidi_t **inputp, snd_rawmidi_t **outputp,
 		fd = snd_open_device(filename, fmode);
 		if (fd < 0) {
 			snd_ctl_close(ctl);
-			SYSERR("open %s failed", filename);
+			SYSMSG("open %s failed", filename);
 			return -errno;
 		}
 	}
 	if (ioctl(fd, SNDRV_RAWMIDI_IOCTL_PVERSION, &ver) < 0) {
 		ret = -errno;
-		SYSERR("SNDRV_RAWMIDI_IOCTL_PVERSION failed");
+		SYSMSG("SNDRV_RAWMIDI_IOCTL_PVERSION failed");
 		close(fd);
 		snd_ctl_close(ctl);
 		return ret;
@@ -406,7 +406,7 @@ int snd_rawmidi_hw_open(snd_rawmidi_t **inputp, snd_rawmidi_t **outputp,
 		memset(&info, 0, sizeof(info));
 		info.stream = outputp ? SNDRV_RAWMIDI_STREAM_OUTPUT : SNDRV_RAWMIDI_STREAM_INPUT;
 		if (ioctl(fd, SNDRV_RAWMIDI_IOCTL_INFO, &info) < 0) {
-			SYSERR("SNDRV_RAWMIDI_IOCTL_INFO failed");
+			SYSMSG("SNDRV_RAWMIDI_IOCTL_INFO failed");
 			ret = -errno;
 			close(fd);
 			snd_ctl_close(ctl);
