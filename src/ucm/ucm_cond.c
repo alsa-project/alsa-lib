alsa-project/alsa-lib

commit ef6463a20914e4cdcd22917d3206a06b86718c3f

ucm: fix Path condition - substitute Path and Mode fields
The Path and Mode fields should be also substituted for
the runtime evaluation. See Fixes.

Fixes: https://github.com/alsa-project/alsa-lib/issues/395
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/ucm/ucm_cond.c
@@ -274,6 +274,7 @@ static int if_eval_path(snd_use_case_mgr_t *uc_mgr, snd_config_t *eval)
 {
 	const char *path, *mode = "";
 	int err, amode = F_OK;
+	char *s;
 
 	if (uc_mgr->conf_format < 4) {
 		uc_error("Path condition is supported in v4+ syntax");
@@ -292,27 +293,34 @@ static int if_eval_path(snd_use_case_mgr_t *uc_mgr, snd_config_t *eval)
 		return -EINVAL;
 	}
 
-	if (strncasecmp(mode, "exist", 5) == 0) {
+	err = uc_mgr_get_substituted_value(uc_mgr, &s, mode);
+	if (err < 0)
+		return err;
+	if (strncasecmp(s, "exist", 5) == 0) {
 		amode = F_OK;
-	} else if (strcasecmp(mode, "read") == 0) {
+	} else if (strcasecmp(s, "read") == 0) {
 		amode = R_OK;
-	} else if (strcasecmp(mode, "write") == 0) {
+	} else if (strcasecmp(s, "write") == 0) {
 		amode = W_OK;
-	} else if (strcasecmp(mode, "exec") == 0) {
+	} else if (strcasecmp(s, "exec") == 0) {
 		amode = X_OK;
 	} else {
-		uc_error("Path unknown mode (If.Condition.Mode)");
+		uc_error("Path unknown mode '%s' (If.Condition.Mode)", s);
+		free(s);
 		return -EINVAL;
 	}
+	free(s);
 
+	err = uc_mgr_get_substituted_value(uc_mgr, &s, path);
+	if (err < 0)
+		return err;
 #ifdef HAVE_EACCESS
-	if (eaccess(path, amode))
+	err = eaccess(path, amode);
 #else
-	if (access(path, amode))
+	err = access(path, amode);
 #endif
-		return 0;
-
-	return 1;
+	free(s);
+	return err ? 0 : 1;
 }
 
 static int if_eval(snd_use_case_mgr_t *uc_mgr, snd_config_t *eval)

commit 3fd24db22dd3b09fd70de2ae1c58804251737ff4

ucm: fix Path condition - substitute Path
Use the appropriate variable for access/eaccess call.

Fixes: https://github.com/alsa-project/alsa-lib/issues/395
Fixes: ef6463a2 ("ucm: fix Path condition - substitute Path and Mode fields")
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/ucm/ucm_cond.c
@@ -315,9 +315,9 @@ static int if_eval_path(snd_use_case_mgr_t *uc_mgr, snd_config_t *eval)
 	if (err < 0)
 		return err;
 #ifdef HAVE_EACCESS
-	err = eaccess(path, amode);
+	err = eaccess(s, amode);
 #else
-	err = access(path, amode);
+	err = access(s, amode);
 #endif
 	free(s);
 	return err ? 0 : 1;