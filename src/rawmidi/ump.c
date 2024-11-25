alsa-project/alsa-lib

commit 76d2d285c10b1e368b4bf9494cf9270ae46197cb

rawmidi: ump - fix snd_ump_block_info_get_block_id double version
The changes for 1.2.13 in Versions.in.in file matches also old
1.2.10 function snd_ump_block_info_get_block_id:

1 Removed function:

  [D] 'function void snd_ump_block_info_set_block_id(snd_ump_block_info_t*, unsigned int)'    {snd_ump_block_info_set_block_id@@ALSA_1.2.10}

Add 1.2.10 symbol back, but keep 1.2.13 symbol as default.

Closes: https://github.com/alsa-project/alsa-lib/issues/422
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/rawmidi/ump.c
@@ -750,10 +750,19 @@ int snd_ump_block_info_get_device(const snd_ump_block_info_t *info)
  * \param info pointer to a snd_ump_block_info_t structure
  * \return ID number of the given UMP block
  */
+#ifndef DOXYGEN
+EXPORT_SYMBOL unsigned int INTERNAL(snd_ump_block_info_get_block_id)(const snd_ump_block_info_t *info)
+#else
 unsigned int snd_ump_block_info_get_block_id(const snd_ump_block_info_t *info)
+#endif
 {
 	return info->block_id;
 }
+
+#ifndef DOC_HIDDEN
+use_symbol_version(__snd_ump_block_info_get_block_id, snd_ump_block_info_get_block_id, ALSA_1.2.10);
+use_default_symbol_version(__snd_ump_block_info_get_block_id, snd_ump_block_info_get_block_id, ALSA_1.2.13);
+#endif /* DOC_HIDDEN */
 
 /**
  * \brief get UMP block activeness

commit 352cbc5eb94a271a9c3c0ff5bf1742232a69e0d0

rawmidi: ump - fix snd_ump_block_info_set_block_id double version
Fix mistake snd_ump_block_info_get_block_id / snd_ump_block_info_set_block_id .

Fixes: 76d2d285 ("rawmidi: ump - fix snd_ump_block_info_get_block_id double version")
Link: https://github.com/alsa-project/alsa-lib/issues/422
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/rawmidi/ump.c
@@ -750,19 +750,10 @@ int snd_ump_block_info_get_device(const snd_ump_block_info_t *info)
  * \param info pointer to a snd_ump_block_info_t structure
  * \return ID number of the given UMP block
  */
-#ifndef DOXYGEN
-EXPORT_SYMBOL unsigned int INTERNAL(snd_ump_block_info_get_block_id)(const snd_ump_block_info_t *info)
-#else
 unsigned int snd_ump_block_info_get_block_id(const snd_ump_block_info_t *info)
-#endif
 {
 	return info->block_id;
 }
-
-#ifndef DOC_HIDDEN
-use_symbol_version(__snd_ump_block_info_get_block_id, snd_ump_block_info_get_block_id, ALSA_1.2.10);
-use_default_symbol_version(__snd_ump_block_info_get_block_id, snd_ump_block_info_get_block_id, ALSA_1.2.13);
-#endif /* DOC_HIDDEN */
 
 /**
  * \brief get UMP block activeness
@@ -881,11 +872,21 @@ void snd_ump_block_info_set_device(snd_ump_block_info_t *info, unsigned int devi
  *
  * This function is mostly used for setting the block ID to query.
  */
+#ifndef DOXYGEN
+EXPORT_SYMBOL void INTERNAL(snd_ump_block_info_set_block_id)(snd_ump_block_info_t *info,
+							     unsigned int id)
+#else
 void snd_ump_block_info_set_block_id(snd_ump_block_info_t *info,
 				     unsigned int id)
+#endif
 {
 	info->block_id = id;
 }
+
+#ifndef DOC_HIDDEN
+use_symbol_version(__snd_ump_block_info_set_block_id, snd_ump_block_info_set_block_id, ALSA_1.2.10);
+use_default_symbol_version(__snd_ump_block_info_set_block_id, snd_ump_block_info_set_block_id, ALSA_1.2.13);
+#endif /* DOC_HIDDEN */
 
 /**
  * \brief set activeness to snd_ump_block_info_t structure