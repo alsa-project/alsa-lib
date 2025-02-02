alsa-project/alsa-lib

commit ea8972c83b020d92e1a9f0a5c12eaee159bf6c63

include: prefer alsa/asoundlib.h for apps, dependency cleanups
Fixes several issues with header files:

- prefer alsa/asoundlib.h file for the alsa-lib core functionalities
  (use #warning to inform current and future developers, do the job)
- include alsa/asoundlib.h in headers for external plugins by default
- pcm_external.h: dependencies cleanup
- as benefit, the parsers in IDEs should get all information for individial
  header files (see PR#435)

This change was mainly tergetted to fix errors caused by wrong include order
(like for endianness detection, missing typedefs etc.).

Closes: https://github.com/alsa-project/alsa-lib/issues/431
Link: https://github.com/alsa-project/alsa-lib/pull/435
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

include/pcm_extplug.h
@@ -28,6 +28,11 @@
  *
  */
 
+#ifndef __ALSA_PCM_EXTERNAL_H
+#warning "use #include <alsa/pcm_external.h>, <alsa/pcm_extplug.h> should not be used directly"
+#include <alsa/pcm_external.h>
+#endif
+
 #ifndef __ALSA_PCM_EXTPLUG_H
 #define __ALSA_PCM_EXTPLUG_H
 

include/pcm_ioplug.h
@@ -28,6 +28,11 @@
  *
  */
 
+#ifndef __ALSA_PCM_EXTERNAL_H
+#warning "use #include <alsa/pcm_external.h>, <alsa/pcm_ioplug.h> should not be used directly"
+#include <alsa/pcm_external.h>
+#endif
+
 #ifndef __ALSA_PCM_IOPLUG_H
 #define __ALSA_PCM_IOPLUG_H
 

configure.ac
@@ -634,6 +634,7 @@ AM_CONDITIONAL([BUILD_PCM_PLUGIN_DSNOOP], [test x$build_pcm_dsnoop = xyes])
 AM_CONDITIONAL([BUILD_PCM_PLUGIN_ASYM], [test x$build_pcm_asym = xyes])
 AM_CONDITIONAL([BUILD_PCM_PLUGIN_IEC958], [test x$build_pcm_iec958 = xyes])
 AM_CONDITIONAL([BUILD_PCM_PLUGIN_SOFTVOL], [test x$build_pcm_softvol = xyes])
+AM_CONDITIONAL([BUILD_PCM_PLUGIN_EXTERNAL], [test x$build_pcm_extplug = xyes -o x$build_pcm_ioplug = xyes])
 AM_CONDITIONAL([BUILD_PCM_PLUGIN_EXTPLUG], [test x$build_pcm_extplug = xyes])
 AM_CONDITIONAL([BUILD_PCM_PLUGIN_IOPLUG], [test x$build_pcm_ioplug = xyes])
 AM_CONDITIONAL([BUILD_PCM_PLUGIN_MMAP_EMUL], [test x$build_pcm_mmap_emul = xyes])
@@ -826,6 +827,7 @@ test "$build_pcm" = "yes" && echo "#include <alsa/timer.h>" >> include/asoundlib
 test "$build_hwdep" = "yes" && echo "#include <alsa/hwdep.h>" >> include/asoundlib.h
 echo "#include <alsa/control.h>" >> include/asoundlib.h
 test "$build_mixer" = "yes" && echo "#include <alsa/mixer.h>" >> include/asoundlib.h
+test "$build_seq" = "yes" && echo "#include <alsa/ump_msg.h>" >> include/asoundlib.h
 test "$build_seq" = "yes" && echo "#include <alsa/seq_event.h>" >> include/asoundlib.h
 test "$build_seq" = "yes" && echo "#include <alsa/seq.h>" >> include/asoundlib.h
 test "$build_seq" = "yes" && echo "#include <alsa/seqmid.h>" >> include/asoundlib.h

include/Makefile.am
@@ -22,15 +22,16 @@ endif
 if BUILD_PCM_PLUGIN_RATE
 alsainclude_HEADERS += pcm_rate.h
 endif
-if BUILD_PCM_PLUGIN_EXTPLUG
-alsainclude_HEADERS += pcm_external.h pcm_extplug.h
+if BUILD_PCM_PLUGIN_EXTERNAL
+# FIXME: pcm_external.h includes both pcm_extplug.h and pcm_ioplug.h
+alsainclude_HEADERS += pcm_external.h pcm_extplug.h pcm_ioplug.h
 endif
-if BUILD_PCM_PLUGIN_IOPLUG
-if !BUILD_PCM_PLUGIN_EXTPLUG
-alsainclude_HEADERS += pcm_external.h
-endif
-alsainclude_HEADERS += pcm_ioplug.h
-endif
+#if BUILD_PCM_PLUGIN_EXTPLUG
+#alsainclude_HEADERS += pcm_extplug.h
+#endif
+#if BUILD_PCM_PLUGIN_IOPLUG
+#alsainclude_HEADERS += pcm_ioplug.h
+#endif
 endif
 
 if BUILD_RAWMIDI

include/asoundef.h
@@ -25,6 +25,12 @@
  *
  */
 
+#if !defined(__ASOUNDLIB_H) && !defined(ALSA_LIBRARY_BUILD)
+/* don't use ALSA_LIBRARY_BUILD define in sources outside alsa-lib */
+#warning "use #include <alsa/asoundlib.h>, <alsa/asoundef.h> should not be used directly"
+#include <alsa/asoundlib.h>
+#endif
+
 #ifndef __ALSA_ASOUNDEF_H
 #define __ALSA_ASOUNDEF_H
 

include/asoundlib-head.h
@@ -38,3 +38,5 @@
 #include <poll.h>
 #include <errno.h>
 #include <stdarg.h>
+#include <stdint.h>
+#include <time.h>

include/conf.h
@@ -25,6 +25,11 @@
  *
  */
 
+#if !defined(__ASOUNDLIB_H) && !defined(ALSA_LIBRARY_BUILD)
+/* don't use ALSA_LIBRARY_BUILD define in sources outside alsa-lib */
+#include <alsa/asoundlib.h>
+#endif
+
 #ifndef __ALSA_CONF_H
 #define __ALSA_CONF_H
 

include/control.h
@@ -25,6 +25,12 @@
  *
  */
 
+#if !defined(__ASOUNDLIB_H) && !defined(ALSA_LIBRARY_BUILD)
+/* don't use ALSA_LIBRARY_BUILD define in sources outside alsa-lib */
+#warning "use #include <alsa/asoundlib.h>, <alsa/control.h> should not be used directly"
+#include <alsa/asoundlib.h>
+#endif
+
 #ifndef __ALSA_CONTROL_H
 #define __ALSA_CONTROL_H
 

commit 995e279081958cd292fe8515075b21df7cac85ae

include: pcm extplug/ioplug: fix internal include
The header files are directly included when the alsa-lib is building.
Do not show warning.

Fixes: ea8972c8 ("include: prefer alsa/asoundlib.h for apps, dependency cleanups")
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

include/pcm_extplug.h
@@ -28,7 +28,7 @@
  *
  */
 
-#ifndef __ALSA_PCM_EXTERNAL_H
+#if !defined(__ALSA_PCM_EXTERNAL_H) && !defined(ALSA_LIBRARY_BUILD)
 #warning "use #include <alsa/pcm_external.h>, <alsa/pcm_extplug.h> should not be used directly"
 #include <alsa/pcm_external.h>
 #endif

include/pcm_ioplug.h
@@ -28,7 +28,7 @@
  *
  */
 
-#ifndef __ALSA_PCM_EXTERNAL_H
+#if !defined(__ALSA_PCM_EXTERNAL_H) && !defined(ALSA_LIBRARY_BUILD)
 #warning "use #include <alsa/pcm_external.h>, <alsa/pcm_ioplug.h> should not be used directly"
 #include <alsa/pcm_external.h>
 #endif