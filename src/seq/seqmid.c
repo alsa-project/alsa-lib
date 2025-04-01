90eb3c1e960bbb69ba1911a82f13a4c0099cf1d9
Takashi Iwai <tiwai@suse.de>
2025-04-01 08:02:51 +0200

    ALSA: seq: Use SND_* instead of SNDRV_*
    The values SNDRV_XXX are used incorrectly in some code where they
    should have been SND_XXX, due to copy&paste from the kernel code.
    
    Practically seen there are no difference, and the code still works
    fine, but those should be corrected for consistency.
    
    Fixes: 6167b8ce3e80 ("seq: Add API helper functions for creating UMP Endpoint and Blocks")
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- src/seq/seqmid.c
+++ src/seq/seqmid.c
@@ -571,18 +571,18 @@ int snd_seq_create_ump_endpoint(snd_seq_t *seq,
 	snd_seq_port_info_set_name(pinfo, "MIDI 2.0");
 	snd_seq_port_info_set_capability(pinfo,
 					 SND_SEQ_PORT_CAP_UMP_ENDPOINT |
-					 SNDRV_SEQ_PORT_CAP_READ |
-					 SNDRV_SEQ_PORT_CAP_SYNC_READ |
-					 SNDRV_SEQ_PORT_CAP_SUBS_READ |
-					 SNDRV_SEQ_PORT_CAP_WRITE |
-					 SNDRV_SEQ_PORT_CAP_SYNC_WRITE |
-					 SNDRV_SEQ_PORT_CAP_SUBS_WRITE |
-					 SNDRV_SEQ_PORT_CAP_DUPLEX);
+					 SND_SEQ_PORT_CAP_READ |
+					 SND_SEQ_PORT_CAP_SYNC_READ |
+					 SND_SEQ_PORT_CAP_SUBS_READ |
+					 SND_SEQ_PORT_CAP_WRITE |
+					 SND_SEQ_PORT_CAP_SYNC_WRITE |
+					 SND_SEQ_PORT_CAP_SUBS_WRITE |
+					 SND_SEQ_PORT_CAP_DUPLEX);
 	snd_seq_port_info_set_type(pinfo,
 				   SND_SEQ_PORT_TYPE_MIDI_GENERIC |
-				   SNDRV_SEQ_PORT_TYPE_MIDI_UMP |
+				   SND_SEQ_PORT_TYPE_MIDI_UMP |
 				   SND_SEQ_PORT_TYPE_APPLICATION |
-				   SNDRV_SEQ_PORT_TYPE_PORT);
+				   SND_SEQ_PORT_TYPE_PORT);
 	snd_seq_port_info_set_ump_group(pinfo, 0);
 	err = snd_seq_create_port(seq, pinfo);
 	if (err < 0) {
@@ -643,24 +643,24 @@ static void update_group_ports(snd_seq_t *seq, snd_ump_endpoint_info_t *ep)
 			    i >= bp->first_group + bp->num_groups)
 				continue;
 			switch (bp->direction) {
-			case SNDRV_UMP_DIR_INPUT: /* sink, receiver */
-				caps |= SNDRV_SEQ_PORT_CAP_WRITE |
-					SNDRV_SEQ_PORT_CAP_SYNC_WRITE |
-					SNDRV_SEQ_PORT_CAP_SUBS_WRITE;
+			case SND_UMP_DIR_INPUT: /* sink, receiver */
+				caps |= SND_SEQ_PORT_CAP_WRITE |
+					SND_SEQ_PORT_CAP_SYNC_WRITE |
+					SND_SEQ_PORT_CAP_SUBS_WRITE;
 				break;
-			case SNDRV_UMP_DIR_OUTPUT: /* source, transmitter */
-				caps |= SNDRV_SEQ_PORT_CAP_READ |
-					SNDRV_SEQ_PORT_CAP_SYNC_READ |
-					SNDRV_SEQ_PORT_CAP_SUBS_READ;
+			case SND_UMP_DIR_OUTPUT: /* source, transmitter */
+				caps |= SND_SEQ_PORT_CAP_READ |
+					SND_SEQ_PORT_CAP_SYNC_READ |
+					SND_SEQ_PORT_CAP_SUBS_READ;
 				break;
-			case SNDRV_UMP_DIR_BIDIRECTION:
-				caps |= SNDRV_SEQ_PORT_CAP_READ |
-					SNDRV_SEQ_PORT_CAP_SYNC_READ |
-					SNDRV_SEQ_PORT_CAP_SUBS_READ |
-					SNDRV_SEQ_PORT_CAP_WRITE |
-					SNDRV_SEQ_PORT_CAP_SYNC_WRITE |
-					SNDRV_SEQ_PORT_CAP_SUBS_WRITE |
-					SNDRV_SEQ_PORT_CAP_DUPLEX;
+			case SND_UMP_DIR_BIDIRECTION:
+				caps |= SND_SEQ_PORT_CAP_READ |
+					SND_SEQ_PORT_CAP_SYNC_READ |
+					SND_SEQ_PORT_CAP_SUBS_READ |
+					SND_SEQ_PORT_CAP_WRITE |
+					SND_SEQ_PORT_CAP_SYNC_WRITE |
+					SND_SEQ_PORT_CAP_SUBS_WRITE |
+					SND_SEQ_PORT_CAP_DUPLEX;
 				break;
 			}
 
