alsa-project/alsa-lib

commit 352cbc5eb94a271a9c3c0ff5bf1742232a69e0d0

rawmidi: ump - fix snd_ump_block_info_set_block_id double version
Fix mistake snd_ump_block_info_get_block_id / snd_ump_block_info_set_block_id .

Fixes: 76d2d285 ("rawmidi: ump - fix snd_ump_block_info_get_block_id double version")
Link: https://github.com/alsa-project/alsa-lib/issues/422
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/rawmidi/ump.c
@@ -750,19 +750,10 @@ int snd_ump_block_info_get_device(const snd_ump_block_info_t *info)
  * \param info pointer to a snd_ump_block_info_t structure
  * \return ID number of the given UMP block
  */
-#ifndef DOXYGEN
-EXPORT_SYMBOL unsigned int INTERNAL(snd_ump_block_info_get_block_id)(const snd_ump_block_info_t *info)
-#else
 unsigned int snd_ump_block_info_get_block_id(const snd_ump_block_info_t *info)
-#endif
 {
 	return info->block_id;
 }
-
-#ifndef DOC_HIDDEN
-use_symbol_version(__snd_ump_block_info_get_block_id, snd_ump_block_info_get_block_id, ALSA_1.2.10);
-use_default_symbol_version(__snd_ump_block_info_get_block_id, snd_ump_block_info_get_block_id, ALSA_1.2.13);
-#endif /* DOC_HIDDEN */
 
 /**
  * \brief get UMP block activeness
@@ -881,11 +872,21 @@ void snd_ump_block_info_set_device(snd_ump_block_info_t *info, unsigned int devi
  *
  * This function is mostly used for setting the block ID to query.
  */
+#ifndef DOXYGEN
+EXPORT_SYMBOL void INTERNAL(snd_ump_block_info_set_block_id)(snd_ump_block_info_t *info,
+							     unsigned int id)
+#else
 void snd_ump_block_info_set_block_id(snd_ump_block_info_t *info,
 				     unsigned int id)
+#endif
 {
 	info->block_id = id;
 }
+
+#ifndef DOC_HIDDEN
+use_symbol_version(__snd_ump_block_info_set_block_id, snd_ump_block_info_set_block_id, ALSA_1.2.10);
+use_default_symbol_version(__snd_ump_block_info_set_block_id, snd_ump_block_info_set_block_id, ALSA_1.2.13);
+#endif /* DOC_HIDDEN */
 
 /**
  * \brief set activeness to snd_ump_block_info_t structure

commit 6073f53051cc8e9ddb835c787a70b504d4f1ec36

rawmidi: ump - fix snd_ump_block_info_set_block_id double version #2
Define two different symbols with similar functionality.

Fix for:

  CC       ump.lo
/tmp/ccZKscpB.s: Assembler messages:
/tmp/ccZKscpB.s:18: Error: multiple versions [`snd_ump_block_info_set_block_id@@ALSA_1.2.13'|`snd_ump_block_info_set_block_id@ALSA_1.2.10'] for symbol `__snd_ump_block_info_set_block_id'
make[3]: *** [Makefile:392: ump.lo] Error 1

Fixes: 352cbc5e ("rawmidi: ump - fix snd_ump_block_info_set_block_id double version")
Link: https://github.com/alsa-project/alsa-lib/issues/422
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/rawmidi/ump.c
@@ -883,8 +883,16 @@ void snd_ump_block_info_set_block_id(snd_ump_block_info_t *info,
 	info->block_id = id;
 }
 
+#ifndef DOXYGEN
+EXPORT_SYMBOL void INTERNAL(snd_ump_block_info_set_block_id_old)
+				(snd_ump_block_info_t *info, unsigned int id)
+{
+	return INTERNAL(snd_ump_block_info_set_block_id)(info, id);
+}
+#endif
+
 #ifndef DOC_HIDDEN
-use_symbol_version(__snd_ump_block_info_set_block_id, snd_ump_block_info_set_block_id, ALSA_1.2.10);
+use_symbol_version(__snd_ump_block_info_set_block_id_old, snd_ump_block_info_set_block_id, ALSA_1.2.10);
 use_default_symbol_version(__snd_ump_block_info_set_block_id, snd_ump_block_info_set_block_id, ALSA_1.2.13);
 #endif /* DOC_HIDDEN */
 