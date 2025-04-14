alsa-project/alsa-lib

commit a4e47461eca1decd61fbab42690a55358d77804a

seq: update_group_ports - rewrite blknames update
Resolve the warning bellow and use more optimized calls.

seqmid.c: In function ‘update_group_ports’:
seqmid.c:672:45: warning: ‘%s’ directive output may be truncated writing up to 127 bytes into a region of size 61 [-Wformat-truncation=]
  672 |                                          ", %s", bp->name);
      |                                             ^~
seqmid.c:671:33: note: ‘snprintf’ output between 3 and 130 bytes into a destination of size 63
  671 |                                 snprintf(blknames + len, sizeof(blknames) - len,
      |                                 ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  672 |                                          ", %s", bp->name);
      |                                          ~~~~~~~~~~~~~~~~~

Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/seq/seqmid.c
@@ -632,7 +632,6 @@ static void update_group_ports(snd_seq_t *seq, snd_ump_endpoint_info_t *ep)
 		char blknames[64];
 		char name[64];
 		unsigned int caps = 0;
-		int len;
 
 		blknames[0] = 0;
 		for (b = 0; b < ep->num_blocks; b++) {
@@ -664,15 +663,11 @@ static void update_group_ports(snd_seq_t *seq, snd_ump_endpoint_info_t *ep)
 				break;
 			}
 
-			if (!*bp->name)
+			if (bp->name[0] == '\0')
 				continue;
-			len = strlen(blknames);
-			if (len)
-				snprintf(blknames + len, sizeof(blknames) - len,
-					 ", %s", bp->name);
-			else
-				snd_strlcpy(blknames, (const char *)bp->name,
-					    sizeof(blknames));
+			if (blknames[0])
+				snd_strlcpy(blknames, ", ", sizeof(blknames));
+			snd_strlcpy(blknames, (const char *)bp->name, sizeof(blknames));
 		}
 
 		if (!*blknames)

commit c748afe123b3e9fdd020853d508b33beff80afa5

seq: seqmid - use correct snd_strlcat instead snd_strlcpy in update_group_ports()
Fixes: a4e47461 ("seq: update_group_ports - rewrite blknames update")
Suggested-by: Takashi Iwai <tiwai@suse.de>
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/seq/seqmid.c
@@ -666,8 +666,8 @@ static void update_group_ports(snd_seq_t *seq, snd_ump_endpoint_info_t *ep)
 			if (bp->name[0] == '\0')
 				continue;
 			if (blknames[0])
-				snd_strlcpy(blknames, ", ", sizeof(blknames));
-			snd_strlcpy(blknames, (const char *)bp->name, sizeof(blknames));
+				snd_strlcat(blknames, ", ", sizeof(blknames));
+			snd_strlcat(blknames, (const char *)bp->name, sizeof(blknames));
 		}
 
 		if (!*blknames)