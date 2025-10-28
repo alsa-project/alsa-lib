e6d0db9d0c9160d880861138d1a270cfef70dd26
Takashi Iwai <tiwai@suse.de>
2025-10-28 13:34:10 +0100

    rawmidi: Fix the prefix of the inactive stream flag
    I copied SNDRV_RAWMIDI_INFO_STREAM_INACTIVE definition as is from the
    kernel uapi header, but for alsa-lib, it should have been changed to
    SND_ prefix instead.
    
    Put the correct definition while keeping the old SNDRV_ definition for
    compatibility (that might be already used by applications).
    
    Fixes: 137eca7720be ("rawmidi: Extensions for tied device and substream inactive flag")
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- include/rawmidi.h
+++ include/rawmidi.h
@@ -101,7 +101,8 @@ typedef enum _snd_rawmidi_read_mode {
 
 /** rawmidi info bit flags */
 #define SND_RAWMIDI_INFO_UMP			0x00000008	/**< rawmidi is UMP */
-#define SNDRV_RAWMIDI_INFO_STREAM_INACTIVE	0x00000010	/**< the selected substream is inactive */
+#define SND_RAWMIDI_INFO_STREAM_INACTIVE	0x00000010	/**< the selected substream is inactive */
+#define SNDRV_RAWMIDI_INFO_STREAM_INACTIVE	SND_RAWMIDI_INFO_STREAM_INACTIVE /* only for compatibility */
 
 int snd_rawmidi_open(snd_rawmidi_t **in_rmidi, snd_rawmidi_t **out_rmidi,
 		     const char *name, int mode);
