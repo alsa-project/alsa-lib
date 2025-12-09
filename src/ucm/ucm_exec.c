alsa-project/alsa-lib

commit a068cf08ad67447893b707cddfce31c9cafee643

ucm: use closefrom instead of close_range
closefrom is a library function with a fallback mechanism for when the
kernel does not support the close_range syscall.

Also check for the function properly instead of assuming it is available
with _GNU_SOURCE defined.

Closes: https://github.com/alsa-project/alsa-lib/pull/486
Fixes: https://github.com/alsa-project/alsa-lib/issues/485
Signed-off-by: Mike Gilbert <floppym@gentoo.org>
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/ucm/ucm_exec.c
@@ -259,8 +259,8 @@ int uc_mgr_exec(const char *prog)
 
 		close(f);
 
-#if defined(_GNU_SOURCE)
-		close_range(3, maxfd, 0);
+#if HAVE_DECL_CLOSEFROM
+		closefrom(3);
 #else
 		for (f = 3; f < maxfd; f++)
 			close(f);

configure.ac
@@ -52,6 +52,7 @@ dnl Checks for library functions.
 AC_PROG_GCC_TRADITIONAL
 AC_CHECK_FUNCS([uselocale])
 AC_CHECK_FUNCS([eaccess])
+AC_CHECK_DECLS([closefrom])
 
 dnl Enable largefile support
 AC_SYS_LARGEFILE

commit 813ffe34ff6c720dcc56e4549338bf9e9184af1f

ucm: exec - fix maxfd used warning
Fixes: a068cf08 ("ucm: use closefrom instead of close_range")
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/ucm/ucm_exec.c
@@ -183,7 +183,7 @@ static int parse_args(char ***argv, int argc, const char *cmd)
  */
 int uc_mgr_exec(const char *prog)
 {
-	pid_t p, f, maxfd;
+	pid_t p, f;
 	int err = 0, status;
 	char bin[PATH_MAX];
 	struct sigaction sa;
@@ -211,8 +211,6 @@ int uc_mgr_exec(const char *prog)
 		}
 		prog = bin;
 	}
-
-	maxfd = sysconf(_SC_OPEN_MAX);
 
 	/*
 	 * block SIGCHLD signal
@@ -262,8 +260,11 @@ int uc_mgr_exec(const char *prog)
 #if HAVE_DECL_CLOSEFROM
 		closefrom(3);
 #else
-		for (f = 3; f < maxfd; f++)
-			close(f);
+		{
+			pid_t maxfd = sysconf(_SC_OPEN_MAX);
+			for (f = 3; f < maxfd; f++)
+				close(f);
+		}
 #endif
 
 		/* install default handlers for the forked process */