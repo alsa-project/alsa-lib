alsa-project/alsa-lib

commit 8f5779eb3fe43ba8b61490db5292720413fb2d31

ucm: add LibraryConfig support
This commit allows to define private alsa-lib's configuration. When
the configuration is present, the device values ("PlaybackCTL",
"CaptureCTL", "PlaybackMixer", "CaptureMixer", "CapturePCM")
are prefixed with '_ucmHEXA.' string where HEXA is replaced by the
unique hexadecimal number identifying the opened ucm manager handle.

    Syntax 4

    LibraryConfig.a_label.SubstiConfig {
            # substituted library configuration like:
            usr_share_dir "${ConfLibDir}"
    }

    LibraryConfig.b_label.Config {
            # non-substituted library configuration like:
            usr_share_dir "/usr/share/alsa"
    }

    The File counterparts:

    LibraryConfig.c_label.SubstiFile "/some/path"
    LibraryConfig.d_label.File "/some/path"

Note that for files the contents is substituted on the request,
but the file name is always substituted (useful for ${ConfDir} etc.).

The private configuration is not saved or preserved. It's life time
belongs to the opened ucm manager handle.

Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/ucm/parser.c
@@ -31,6 +31,7 @@
  */
 
 #include "ucm_local.h"
+#include <stdbool.h>
 #include <dirent.h>
 #include <limits.h>
 
@@ -417,6 +418,128 @@ int uc_mgr_evaluate_inplace(snd_use_case_mgr_t *uc_mgr,
 		if (err3 < 0)
 			return err3;
 	}
+	return 0;
+}
+
+/*
+ * Parse one item for alsa-lib config
+ */
+static int parse_libconfig1(snd_use_case_mgr_t *uc_mgr, snd_config_t *cfg)
+{
+	snd_config_iterator_t i, next;
+	snd_config_t *n, *config = NULL;
+	const char *id, *file = NULL;
+	bool substfile = false, substconfig = false;
+	int err;
+
+	if (snd_config_get_id(cfg, &id) < 0)
+		return -EINVAL;
+
+	if (snd_config_get_type(cfg) != SND_CONFIG_TYPE_COMPOUND) {
+		uc_error("compound type expected for %s", id);
+		return -EINVAL;
+	}
+
+	snd_config_for_each(i, next, cfg) {
+		n = snd_config_iterator_entry(i);
+
+		if (snd_config_get_id(n, &id) < 0)
+			return -EINVAL;
+
+		if (strcmp(id, "File") == 0 ||
+		    strcmp(id, "SubstiFile") == 0) {
+			substfile = id[0] == 'S';
+			err = snd_config_get_string(n, &file);
+			if (err < 0)
+				return err;
+			continue;
+		}
+
+		if (strcmp(id, "Config") == 0 ||
+		    strcmp(id, "SubstiConfig") == 0) {
+			substconfig = id[0] == 'S';
+			if (snd_config_get_type(n) != SND_CONFIG_TYPE_COMPOUND)
+				return -EINVAL;
+			config = n;
+			continue;
+		}
+
+		uc_error("unknown field %s", id);
+		return -EINVAL;
+	}
+
+	if (file) {
+		if (substfile) {
+			snd_config_t *cfg;
+			err = uc_mgr_config_load(uc_mgr->conf_format, file, &cfg);
+			if (err < 0)
+				return err;
+			err = uc_mgr_substitute_tree(uc_mgr, cfg);
+			if (err < 0) {
+				snd_config_delete(config);
+				return err;
+			}
+			err = snd_config_merge(uc_mgr->local_config, cfg, 1);
+			if (err < 0) {
+				snd_config_delete(cfg);
+				return err;
+			}
+		} else {
+			char filename[PATH_MAX];
+
+			ucm_filename(filename, sizeof(filename), uc_mgr->conf_format,
+				     file[0] == '/' ? NULL : uc_mgr->conf_dir_name,
+				     file);
+			err = uc_mgr_config_load_into(uc_mgr->conf_format, filename, uc_mgr->local_config);
+			if (err < 0)
+				return err;
+		}
+	}
+
+	if (config) {
+		if (substconfig) {
+			err = uc_mgr_substitute_tree(uc_mgr, config);
+			if (err < 0) {
+				snd_config_delete(config);
+				return err;
+			}
+		}
+		err = snd_config_merge(uc_mgr->local_config, config, 1);
+		if (err < 0) {
+			snd_config_delete(config);
+			return err;
+		}
+	}
+
+	return 0;
+}
+
+/*
+ * Parse alsa-lib config
+ */
+static int parse_libconfig(snd_use_case_mgr_t *uc_mgr, snd_config_t *cfg)
+{
+	snd_config_iterator_t i, next;
+	snd_config_t *n;
+	const char *id;
+	int err;
+
+	if (snd_config_get_id(cfg, &id) < 0)
+		return -EINVAL;
+
+	if (snd_config_get_type(cfg) != SND_CONFIG_TYPE_COMPOUND) {
+		uc_error("compound type expected for %s", id);
+		return -EINVAL;
+	}
+
+	snd_config_for_each(i, next, cfg) {
+		n = snd_config_iterator_entry(i);
+
+		err = parse_libconfig1(uc_mgr, n);
+		if (err < 0)
+			return err;
+	}
+
 	return 0;
 }
 
@@ -1644,6 +1767,7 @@ static int parse_verb_file(snd_use_case_mgr_t *uc_mgr,
 						file);
 				goto _err;
 			}
+			continue;
 		}
 
 		/* device remove */
@@ -1654,6 +1778,17 @@ static int parse_verb_file(snd_use_case_mgr_t *uc_mgr,
 						file);
 				goto _err;
 			}
+			continue;
+		}
+
+		/* alsa-lib configuration */
+		if (uc_mgr->conf_format > 3 && strcmp(id, "LibraryConfig") == 0) {
+			err = parse_libconfig(uc_mgr, n);
+			if (err < 0) {
+				uc_error("error: failed to parse LibConfig");
+				return err;
+			}
+			continue;
 		}
 	}
 
@@ -1957,6 +2092,16 @@ static int parse_master_file(snd_use_case_mgr_t *uc_mgr, snd_config_t *cfg)
 			err = parse_value(uc_mgr, &uc_mgr->value_list, n);
 			if (err < 0) {
 				uc_error("error: failed to parse ValueDefaults");
+				return err;
+			}
+			continue;
+		}
+
+		/* alsa-lib configuration */
+		if (uc_mgr->conf_format > 3 && strcmp(id, "LibraryConfig") == 0) {
+			err = parse_libconfig(uc_mgr, n);
+			if (err < 0) {
+				uc_error("error: failed to parse LibraryConfig");
 				return err;
 			}
 			continue;

include/local.h
@@ -374,4 +374,11 @@ int _snd_config_load_with_include(snd_config_t *config, snd_input_t *in,
 void *INTERNAL(snd_dlopen)(const char *name, int mode, char *errbuf, size_t errbuflen);
 #endif
 
+const char *uc_mgr_alibcfg_by_device(snd_config_t **config, const char *name);
+
+static inline int _snd_is_ucm_device(const char *name)
+{
+	return name && name[0] == '_' && name[1] == 'u' && name[2] == 'c' && name[3] == 'm';
+}
+
 #endif

include/use-case.h
@@ -257,6 +257,7 @@ int snd_use_case_get_list(snd_use_case_mgr_t *uc_mgr,
  *   - NULL 		- return current card
  *   - _verb		- return current verb
  *   - _file		- return configuration file loaded for current card
+ *   - _alibcfg		- return private alsa-lib's configuration for current card
  *
  *   - [=]{NAME}[/[{modifier}|{/device}][/{verb}]]
  *                      - value identifier {NAME}

src/control/control.c
@@ -1520,9 +1520,15 @@ int snd_ctl_open(snd_ctl_t **ctlp, const char *name, int mode)
 	int err;
 
 	assert(ctlp && name);
-	err = snd_config_update_ref(&top);
-	if (err < 0)
-		return err;
+	if (_snd_is_ucm_device(name)) {
+		name = uc_mgr_alibcfg_by_device(&top, name);
+		if (name == NULL)
+			return -ENODEV;
+	} else {
+		err = snd_config_update_ref(&top);
+		if (err < 0)
+			return err;
+	}
 	err = snd_ctl_open_noupdate(ctlp, top, name, mode, 0);
 	snd_config_unref(top);
 	return err;

src/pcm/pcm.c
@@ -2686,9 +2686,15 @@ int snd_pcm_open(snd_pcm_t **pcmp, const char *name,
 	int err;
 
 	assert(pcmp && name);
-	err = snd_config_update_ref(&top);
-	if (err < 0)
-		return err;
+	if (_snd_is_ucm_device(name)) {
+		name = uc_mgr_alibcfg_by_device(&top, name);
+		if (name == NULL)
+			return -ENODEV;
+	} else {
+		err = snd_config_update_ref(&top);
+		if (err < 0)
+			return err;
+	}
 	err = snd_pcm_open_noupdate(pcmp, top, name, stream, mode, 0);
 	snd_config_unref(top);
 	return err;

src/rawmidi/rawmidi.c
@@ -304,9 +304,15 @@ int snd_rawmidi_open(snd_rawmidi_t **inputp, snd_rawmidi_t **outputp,
 	int err;
 
 	assert((inputp || outputp) && name);
-	err = snd_config_update_ref(&top);
-	if (err < 0)
-		return err;
+	if (_snd_is_ucm_device(name)) {
+		name = uc_mgr_alibcfg_by_device(&top, name);
+		if (name == NULL)
+			return -ENODEV;
+	} else {
+		err = snd_config_update_ref(&top);
+		if (err < 0)
+			return err;
+	}
 	err = snd_rawmidi_open_noupdate(inputp, outputp, top, name, mode);
 	snd_config_unref(top);
 	return err;

src/seq/seq.c
@@ -978,9 +978,15 @@ int snd_seq_open(snd_seq_t **seqp, const char *name,
 	int err;
 
 	assert(seqp && name);
-	err = snd_config_update_ref(&top);
-	if (err < 0)
-		return err;
+	if (_snd_is_ucm_device(name)) {
+		name = uc_mgr_alibcfg_by_device(&top, name);
+		if (name == NULL)
+			return -ENODEV;
+	} else {
+		err = snd_config_update_ref(&top);
+		if (err < 0)
+			return err;
+	}
 	err = snd_seq_open_noupdate(seqp, top, name, streams, mode, 0);
 	snd_config_unref(top);
 	return err;

src/timer/timer.c
@@ -205,9 +205,15 @@ int snd_timer_open(snd_timer_t **timer, const char *name, int mode)
 	int err;
 
 	assert(timer && name);
-	err = snd_config_update_ref(&top);
-	if (err < 0)
-		return err;
+	if (_snd_is_ucm_device(name)) {
+		name = uc_mgr_alibcfg_by_device(&top, name);
+		if (name == NULL)
+			return -ENODEV;
+	} else {
+		err = snd_config_update_ref(&top);
+		if (err < 0)
+			return err;
+	}
 	err = snd_timer_open_noupdate(timer, top, name, mode);
 	snd_config_unref(top);
 	return err;

commit 7887fbc6f0f660072d1405231703985b72d40cf1

ucm: libconfig parser - fix pathname for substituted file
The path name substituted file contents and normal file contents
should be handled similary. Use correct function determining
the right base directory name.

Fixes: 8f5779eb ("ucm: add LibraryConfig support")
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

src/ucm/parser.c
@@ -804,7 +804,7 @@ static int parse_libconfig1(snd_use_case_mgr_t *uc_mgr, snd_config_t *cfg)
 	if (file) {
 		if (substfile) {
 			snd_config_t *cfg;
-			err = uc_mgr_config_load(uc_mgr->conf_format, file, &cfg);
+			err = uc_mgr_config_load_file(uc_mgr, file, &cfg);
 			if (err < 0)
 				return err;
 			err = uc_mgr_substitute_tree(uc_mgr, cfg);