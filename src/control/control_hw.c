ac4f15b5cd3aa188017f8c16a1d2dd4b75420b4d
Maciej Strozek <mstrozek@opensource.cirrus.com>
2026-02-19 13:46:20 +0000

    control: ucm: add ioctl to retrieve full card components
    The fixed-size components field in SNDRV_CTL_IOCTL_CARD_INFO can be too
    small on systems with many audio devices. The kernel [1] will provide a
    new ioctl to read the full string while truncating the original in
    card_info if it grows too big. Make sure alsa-lib can read the full
    string if the original is truncated.
    
    [1] https://lore.kernel.org/all/20260430110302.3745917-2-mstrozek@opensource.cirrus.com/
    
    Signed-off-by: Maciej Strozek <mstrozek@opensource.cirrus.com>

--- src/control/control_hw.c
+++ src/control/control_hw.c
@@ -129,10 +129,42 @@ static int snd_ctl_hw_subscribe_events(snd_ctl_t *handle, int subscribe)
 static int snd_ctl_hw_card_info(snd_ctl_t *handle, snd_ctl_card_info_t *info)
 {
 	snd_ctl_hw_t *hw = handle->private_data;
-	if (ioctl(hw->fd, SNDRV_CTL_IOCTL_CARD_INFO, info) < 0) {
+	struct snd_ctl_card_info kinfo;
+	struct snd_ctl_card_bytes bytes;
+
+	if (ioctl(hw->fd, SNDRV_CTL_IOCTL_CARD_INFO, &kinfo) < 0) {
 		snd_errornum(CONTROL, "SNDRV_CTL_IOCTL_CARD_INFO failed");
 		return -errno;
 	}
+
+	snd_ctl_card_info_clear(info);
+	info->card = kinfo.card;
+	info->pad = kinfo.pad;
+	snd_strlcpy((char *)info->id, (char *)kinfo.id, sizeof(info->id));
+	snd_strlcpy((char *)info->driver, (char *)kinfo.driver, sizeof(info->driver));
+	snd_strlcpy((char *)info->name, (char *)kinfo.name, sizeof(info->name));
+	snd_strlcpy((char *)info->longname, (char *)kinfo.longname, sizeof(info->longname));
+	snd_strlcpy((char *)info->reserved_, (char *)kinfo.reserved_, sizeof(info->reserved_));
+	snd_strlcpy((char *)info->mixername, (char *)kinfo.mixername, sizeof(info->mixername));
+	if (strnlen((char *)kinfo.components, sizeof(kinfo.components)) == sizeof(kinfo.components) - 1 &&
+	    kinfo.components[sizeof(kinfo.components) - 2] == '>') {
+		bytes.type = SND_CTL_CARD_BTYPE_COMPONENTS;
+		bytes.data = (unsigned char *)info->components;
+		bytes.data_allocated = sizeof(info->components);
+		bytes.data_len = 0;
+		if (ioctl(hw->fd, SNDRV_CTL_IOCTL_CARD_BYTES, &bytes) < 0) {
+			snd_errornum(CONTROL, "SNDRV_CTL_IOCTL_CARD_BYTES failed");
+			return -errno;
+		}
+		if (bytes.data_len != strnlen((char *)bytes.data, sizeof(info->components)) + 1) {
+			snd_error(CONTROL, "SNDRV_CTL_IOCTL_CARD_BYTES unexpected length %u, should be %zu",
+				  bytes.data_len, strnlen((char *)bytes.data, sizeof(info->components)) + 1);
+			return -errno;
+		}
+	} else {
+		snd_strlcpy(info->components, (char *)kinfo.components, sizeof(info->components));
+	}
+
 	return 0;
 }
 
